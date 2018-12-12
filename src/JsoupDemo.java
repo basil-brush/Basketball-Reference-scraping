8626da158d45361503b7380626cb0255b1fbe887
Basil <harry.rodger@yahoo.co.nz>
2018-12-12 15:00:07 +1300

    JsoupDemo creation
    Framework for where the demo will be placed and used. Whether it works or not is yet to be seen.

---
+++ src/JsoupDemo.java
@@ -0,0 +1,14 @@
+/**
+ * Class which uses Jsoup library to scrape data from
+ * https://www.basketball-reference.com/teams/TOR/2019.html
+ * rudimentry beginning
+ * 
+ * @author basil_brush
+ *
+ */
+public class JsoupDemo {
+
+	public static void main(String[] args) {
+		
+	}
+}
