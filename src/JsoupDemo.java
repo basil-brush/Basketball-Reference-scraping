57e47f41cfe28228bb260e5c4d90c13e8f729575
Basil <harry.rodger@yahoo.co.nz>
2018-12-12 15:10:46 +1300

    Document Created and manipulated
    I have created a document by connecting to the toronto raptors URL and acquried that documents title

--- src/JsoupDemo.java
+++ src/JsoupDemo.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  * Class which uses Jsoup library to scrape data from
  * https://www.basketball-reference.com/teams/TOR/2019.html
@@ -8,7 +13,8 @@
  */
 public class JsoupDemo {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		Document doc = Jsoup.connect("https://www.basketball-reference.com/teams/TOR/2019.html").timeout(3000).get();
+		System.out.println(doc.title());
 	}
 }
