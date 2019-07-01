a6622f611ec4aebb065946e2b5ef5bf6dcc4ce55
Basil <harry.rodger@yahoo.co.nz>
2019-07-01 14:30:42 +1200

    Player Scraper updates
    Scraper now accesses all players in NBA for the most current season. It can read through all the relevant information which the player table holds

--- src/Scraper.java
+++ src/Scraper.java
@@ -30,28 +30,35 @@ public class Scraper {
 	 */
 	private static void parseTeams(String teamsWebPage) {
 		try {
+			
 			Document webPage = Jsoup.connect(teamsWebPage).timeout(6000).get();
 
 			Element activeTeamTable = webPage.getElementById("div_teams_active");
-
 			Element activeTeams = activeTeamTable.getElementById("teams_active"); // active teams in the nba
-			Elements teamsData = activeTeams.getElementsByTag("tr");
+			Elements teamsData = activeTeams.getElementsByTag("tr"); //data regarding the franchise
+			
+			//Reads through teams
 			
 			for(Element team: teamsData){
 				
+				String url = null; // holds the url which will be passed to parse players
+
+				
 				if(team.className().equals("full_table")) { //makes sure that it is a current NBA team.
 					
-					for(Element name: team.getElementsByAttribute("href")) {
+					for(Element name: team.getElementsByAttribute("href")) { // reads the team name
 						 
 						System.out.println(name.text());
+						url = name.absUrl("href");
+						
 					}
 					
-					for(Element startYear: team.getElementsByAttributeValue("data-stat", "year_min")) {
+					for(Element startYear: team.getElementsByAttributeValue("data-stat", "year_min")) { // reads the first year the team is in the league
 						 
 						System.out.println(startYear.text());
 					}
 					
-					for(Element gamesPlayer: team.getElementsByAttributeValue("data-stat", "g")) {
+					for(Element gamesPlayer: team.getElementsByAttributeValue("data-stat", "g")) { // reads the amount of games played by a franchise
 						 
 						System.out.println(gamesPlayer.text());
 					}
@@ -91,15 +98,14 @@ public class Scraper {
 						System.out.println(leagueChamps.text());
 					}
 					
-					parsePlayers("TOR");
+					parsePlayers(url);
 				}
 				
 				
 				
 			}
 			
-			//Elements previousTeams = activeTeamTable.getElementsByAttribute("partial_table"); // holds active teams previous "teams"
-			//System.out.println(previousTeams);
+			
 																				
 			
 		} catch (IOException e) {
@@ -110,13 +116,68 @@ public class Scraper {
 	}
 
 	/**
+	 * Parses the relevant values which creates player onjects 
+	 * 
 	 * @param string
 	 */
-	private static void parsePlayers(String string) {
+	private static void parsePlayers(String url) {
 	try{
-		String format = String.format("https://www.basketball-reference.com/teams/%s/2019.html", string);
-		Document doc = Jsoup.connect(format).timeout(6000).get();
 		
+		String format = String.format(url);
+		Document doc = Jsoup.connect(format).get(); //connects to the passed in url to team homepage
+		
+		
+		Element previousTeams = doc.select("table").get(0); //gets the table which holds the relevant teams
+		Element mostRecentTeam = previousTeams.select("tbody").first(); //parses the most recent team
+		Element TeamElements = mostRecentTeam.getElementsByAttribute("href").first(); // reads the team name	
+		String urlTeamPage = TeamElements.absUrl("href"); //gets the url value from the href attribute
+		
+		Document teamPage = Jsoup.connect(urlTeamPage).get(); //opens the page for the most recent team
+		
+		Element playerTable = teamPage.select("table").get(0);
+		Elements rows = playerTable.select("tbody");
+		
+		for(Element player : rows){
+			
+			for(Element playerVals : player.getElementsByTag("tr")) {
+			
+				for(Element number : playerVals.getElementsByAttributeValue("data-stat","number")) {
+					
+				}
+				for(Element name : playerVals.getElementsByAttributeValue("data-stat","player")) {
+					
+				}
+				for(Element position : playerVals.getElementsByAttributeValue("data-stat", "pos")) {
+					
+				}
+				for(Element height : playerVals.getElementsByAttributeValue("data-stat", "height")) {
+					
+				}
+				for(Element weight : playerVals.getElementsByAttributeValue("data-stat", "weight")) {
+					
+				}
+				for(Element birthday : playerVals.getElementsByAttributeValue("data-stat", "birth_date")) {
+					
+				}
+				for(Element birthCountry : playerVals.getElementsByAttributeValue("data-stat", "birth_country")) {
+					
+				}
+				for(Element experience : playerVals.getElementsByAttributeValue("data-stat", "years_experience")) {
+					
+				}
+				for(Element college : playerVals.getElementsByAttributeValue("data-stat", "college")) {
+					
+				}
+
+
+		}
+			
+
+			
+		
+			
+		}
+				
 		
 	} catch (IOException e) {
 
